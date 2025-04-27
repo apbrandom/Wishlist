d1de1d6ebf1b73ba75aa003b030209a14fdb104f
apbrandom <apbrandom@gmail.com>
2025-04-27 21:22:16 +0200

    Initial Commit

---
+++ Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Vadim Vinogradov on 4/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
